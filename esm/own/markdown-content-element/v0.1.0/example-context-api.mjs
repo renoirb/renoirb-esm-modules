d88ef133df17e8c023a88b0e8152c4e9d06b5b0d
Renoir Boulanger <contribs@renoirboulanger.com>
2025-03-18 00:43:37 -0400

    add markdown-content-element@v0.1.0

---
+++ esm/own/markdown-content-element/v0.1.0/example-context-api.mjs
@@ -0,0 +1,48 @@
+import {
+  /*                                  */
+  ContextRequest_MarkdownContent,
+} from '@renoirb/markdown-content-element'
+
+const VERSION_SHOWDOWN = '2.1.0'
+const IMPORT_DEP_SHOWDOWN = `https://ga.jspm.io/npm:showdown@${VERSION_SHOWDOWN}/dist/showdown.js`
+
+// IIFE that creates a cached parser promise
+export const createParser = (() => {
+  let parserPromise = null
+
+  return async (opts = {}) => {
+    if (parserPromise === null) {
+      parserPromise = (async () => {
+        const imported = await import(IMPORT_DEP_SHOWDOWN)
+        const showdown = imported?.default
+        const converter = new showdown.Converter({ metadata: true, ...opts })
+        converter.setOption('openLinksInNewWindow', true)
+        return converter
+      })()
+    }
+    return parserPromise
+  }
+})()
+
+export const contextRequestListener = async (event) => {
+  if (event.context !== ContextRequest_MarkdownContent) {
+    return
+  }
+  event.stopPropagation()
+  const host = event.contextTarget
+  const markdown = host.innerHTML
+  const parser = await createParser()
+  const html = parser.makeHtml(markdown)
+  event.callback({ markdown, html })
+}
+
+if (window?.document?.body) {
+  const currentUrl = new URL(import.meta.url)
+  const hasWorkbenchQuerySearch = currentUrl.searchParams.has('workbench')
+  if (hasWorkbenchQuerySearch) {
+    window.document.body.addEventListener(
+      'context-request',
+      contextRequestListener,
+    )
+  }
+}
