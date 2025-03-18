cb142e022e6fe1d4cdca1f0aeaf5e1c370c642b3
Renoir Boulanger <hello@renoirboulanger.com>
2025-03-18 00:31:26 -0400

    feat(markdown-content-element): New component (#23)

---
+++ packages/markdown-content-element/src/browser/context-api.mjs
@@ -0,0 +1,5 @@
+// Can use Symbol, but let's not make this more complex.
+/**
+ * Context Request event for signaling Markdown needing to be parsed.
+ */
+export const ContextRequest_MarkdownContent = 'markdown-content-context'
---
+++ packages/markdown-content-element/src/browser/index.mjs
@@ -0,0 +1,10 @@
+export * from './context-api.mjs'
+
+import { MarkdownContentElement } from './element.mjs'
+
+export {
+  /*                    */
+  MarkdownContentElement,
+}
+
+export default MarkdownContentElement
