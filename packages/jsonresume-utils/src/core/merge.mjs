14854d79a540b852f68d8b526cb24a3c2257cb68
Renoir Boulanger <contribs@renoirboulanger.com>
2025-08-07 16:25:39 -0400

    feat(jsonresume-utils): merge basics and sort work experience

--- packages/jsonresume-utils/src/core/merge.mjs
+++ packages/jsonresume-utils/src/core/merge.mjs
@@ -7,12 +7,37 @@ const EMPTY_JSON_RESUME = {
   skills: [],
 }
 
+const sortChronologically = (a, b) => {
+  const [leftYear, leftMonth] = a.startDate.split('-')
+  const [rightYear, rightMonth] = b.startDate.split('-')
+
+  let outcome = NaN
+  if (+leftYear < +rightYear) {
+    outcome = 1
+  } else if (+leftYear > +rightYear) {
+    outcome = -1
+  } else {
+    // Year are equals, so, we gotta check for month now
+    if (+leftMonth < +rightMonth) {
+      outcome = 1
+    } else if (+leftMonth > +rightMonth) {
+      outcome = -1
+    } else {
+      outcome = 0
+    }
+  }
+
+  return outcome
+}
+
 export const mergeJsonResume = (overrides, base = EMPTY_JSON_RESUME) => {
   const result = structuredClone({ ...EMPTY_JSON_RESUME, ...base })
 
   if (overrides.work) {
     overrides.work.forEach((override) => {
-      const index = result.work.findIndex((job) => job.startDate === override.startDate)
+      const index = result.work.findIndex(
+        (job) => job.startDate === override.startDate,
+      )
       if (index !== -1) {
         // Merge specific properties
         if (override.highlights !== undefined) {
@@ -24,6 +49,18 @@ export const mergeJsonResume = (overrides, base = EMPTY_JSON_RESUME) => {
       }
     })
   }
+
+  if (Reflect.has(overrides, 'basics')) {
+    const { summary, label } = overrides?.basics ?? {}
+    if (typeof summary === 'string') {
+      Reflect.set(result.basics, 'summary', summary)
+    }
+    if (typeof label === 'string') {
+      Reflect.set(result.basics, 'label', label)
+    }
+  }
+
+  Reflect.set(result, 'work', result.work.sort(sortChronologically))
 
   return result
 }
