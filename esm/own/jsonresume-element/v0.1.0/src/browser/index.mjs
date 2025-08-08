2ead5ce28757d1ed146d5886ddd5098570576835
Renoir Boulanger <contribs@renoirboulanger.com>
2025-08-08 15:02:17 -0400

    add jsonresume-element@v0.1.0

---
+++ esm/own/jsonresume-element/v0.1.0/src/browser/index.mjs
@@ -0,0 +1,12 @@
+export * from './context-api.mjs'
+
+import { JsonResumeElement } from './element.mjs'
+import { WorkExperienceElement } from './work-experience-element.mjs'
+
+export {
+  /*                    */
+  JsonResumeElement,
+  WorkExperienceElement,
+}
+
+export default JsonResumeElement
